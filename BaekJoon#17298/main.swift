854a2c27e0385e5d6a7e6ec00dab360cc89e32c3
Dev_Ozzy <20170926@vision.hoseo.edu>
2023-03-07 02:48:39 +0900

    commit

--- BaekJoon#17298/main.swift
+++ BaekJoon#17298/main.swift
@@ -4,8 +4,31 @@
 //
 //  Created by 김병엽 on 2023/03/07.
 //
+// Reference: https://velog.io/@sun02/Swift-백준-17298-오큰수
 
 import Foundation
 
-print("Hello, World!")
+let N = Int(readLine()!)!
+var array = readLine()!.split(separator: " ").map({Int(String($0))!})
+var stack = [Int]()
 
+func solution() {
+    
+    for i in 0..<N {
+        
+        while !stack.isEmpty && array[stack.last!] < array[i] {
+            array[stack.removeLast()] = array[i]
+        }
+        
+        stack.append(i)
+    }
+    
+    for i in stack {
+        
+        array[i] = -1
+    }
+    
+    print(array.map{String($0)}.joined(separator: " "))
+}
+
+solution()
